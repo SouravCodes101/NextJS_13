c94ba90159a7ad1579ca336a7ecaf95e2592e468
SouravCodes101 <saurabc987@gmail.com>
2023-04-08 23:53:51 +0530

    Next JS 13 Crash Coursegit add .

---
+++ app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const metadata = {
+  title: "About Traversy Media"
+}
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Sourav_Codes 101</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Quam ex, odio sequi quasi minus nostrum cupiditate ratione 
+        sunt error, ut dignissimos veniam voluptas natus, dicta 
+        asperiores ipsa. Delectus, mollitia ex.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Libero aspernatur, accusamus sint excepturi delectus quia 
+        ut totam eos possimus ratione labore accusantium nesciunt 
+        mollitia.Illum veniam aliquid adipisci doloribus velit?</p>
+    </div>
+  )
+}
+
+export default TeamPage
